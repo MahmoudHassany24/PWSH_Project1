5ac0e236433415be3dbdece1b53f3acca46d8b42
MahmoudHassany24 <MahmoudHassany24@github.com>
2025-07-12 18:08:21 +0300

    First lesson PWSH course basic commands

---
+++ Fundamentals.ps1
@@ -0,0 +1,13 @@
+# $PSVersionTable.PSVersion
+# verb + noun
+# Set-Date
+# Set-Service
+# Get-Service
+# Get-Command
+# Get-command -noun Service
+# Get-help Get-Service -Full
+# Get-Alias
+# Set-Location -Path
+# Get-ExecutionPolicy
+# Set-ExecutionPolicy -ExecutionPolicy Restricted -WhatIf
+# Set-ExecutionPolicy -ExecutionPolicy Restricted -Confirm
